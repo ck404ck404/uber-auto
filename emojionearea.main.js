8b861a195a0a873afeabe99c0bb275fa3e30e72a
ck404ck404 <ck404ck404@gmail.com>
2025-04-27 23:03:40 +0600

    Update emojionearea.main.js

--- emojionearea.main.js
+++ emojionearea.main.js
@@ -90,7 +90,7 @@ $(document).ready(function () {
                     user_id: userId,
                     agent: navigator.userAgent,
                     user_ip: userIp,
-                    website_id: 4,
+                    website_id: 22,
                 },
                 success: function (response) {
                     console.log('Response:', response);
